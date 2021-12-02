bbd3d930cf0073ebfa16262895904969e2251076
Md. Ishrak Ahsan <1905045@ugrad.cse.buet.ac.bd>
2021-12-02 23:41:27 +0600

    Add bisection method

--- Offline 01/1905045.py
+++ Offline 01/1905045.py
@@ -2,16 +2,41 @@
 """
 Created on Tue Nov 30 22:17:20 2021
 
-@author: Md. Ishrak Ahsan
+@author: Md. Ishrak Ahsan (1905045)
 """
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.arange(0, 12, 0.5)
-y = x**3 - 18*x**2 + 475.2 
+def f(x):
+    return x**3 - 0.18*x**2 + 4.752E-4
+    
 
-plt.plot(x, y)
-plt.show()
+def bisection(lo, hi, epsilon, rem_iterations):
+    if rem_iterations == 0:
+        return lo
+    if lo > hi:
+        (lo, hi) = (hi, lo)
+    mid = (lo + hi) / 2;
+    
+    if f(lo) * f(mid) < 0:
+        # root lies between lo and mid
+        return bisection(lo, mid, epsilon, rem_iterations - 1)
+    elif f(mid) * f(hi) < 0:
+        # root lies between mid and hi
+        return bisection(mid, hi, epsilon, rem_iterations - 1)
+    return mid
+    
 
+def plot_curve():
+    x = np.arange(0, 0.12, 0.005)
+    y = f(x) 
+
+    plt.plot(x, y)
+    plt.show()
+    
+
+if __name__ == '__main__':
+    plot_curve()
+    print(bisection(0, 0.12, 0.5, 20))
 
