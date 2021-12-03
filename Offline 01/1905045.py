c6eee8cb6ad86fb6626b1a0694197ddc74571c09
Md. Ishrak Ahsan <1905045@ugrad.cse.buet.ac.bd>
2021-12-03 22:10:53 +0600

    Added multiple cols to table

--- Offline 01/1905045.py
+++ Offline 01/1905045.py
@@ -8,11 +8,13 @@ Created on Tue Nov 30 22:17:20 2021
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from IPython.display import HTML
 
 def f(x):
     return x**3 - 0.18*x**2 + 4.752E-4
 
-    
+
+# only find the solution    
 def bisection(lo, hi, eps, max_iterations):
     prev_mid = -1
     for i in range(max_iterations):
@@ -34,11 +36,18 @@ def bisection(lo, hi, eps, max_iterations):
         prev_mid = mid;
 
 
+# show table
 def bisection(lo, hi, iterations):
     prev_mid = -1
-    rows = {'Iteration':[], 'Absolute Relative Approx. Error':[]}
+    rows = {'Iteration':[], 'x_l':[], 'x_u':[], 'x_m':[], '|ε_a|%':[], 'f(x_m)':[]}
     for i in range(iterations):
+        rows['Iteration'].append(i+1)
+        rows['x_l'].append(lo)
+        rows['x_u'].append(hi)
+        
         mid = (lo + hi) / 2
+        rows['x_m'].append(mid)
+        rows['f(x_m)'].append(f(mid))
         
         if f(lo) * f(mid) < 0:
             # root lies between lo and mid
@@ -47,15 +56,17 @@ def bisection(lo, hi, iterations):
             # root lies between mid and hi
             lo = mid
         if i == 0:
+            rows['|ε_a|%'].append('N/A')
             prev_mid = mid
             continue
         err = abs((mid - prev_mid) / mid) * 100
-        rows['Iteration'].append(i+1)
-        rows['Absolute Relative Approx. Error'].append(err)
+        rows['|ε_a|%'].append(err)
+        
         prev_mid = mid;
-    pd.set_option("display.precision", 20)
+    pd.set_option("display.precision", 12)
     df = pd.DataFrame(rows)
     print(df)
+    #display(df)
     return prev_mid
     
 
@@ -64,6 +75,11 @@ def plot_curve():
     y = f(x) 
 
     plt.plot(x, y)
+    
+    plt.xlabel('x from 0 to 0.12')
+    plt.ylabel('x^3 - 0.18x^2 + 0.0004752')
+    plt.title('Plot of f(x) = x^3 - 0.18x^2 + 0.0004752')
+    
     plt.show()
     
 
