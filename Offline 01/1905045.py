749e4d96fb6ed93e039b2e698a79e24d9f41436a
Md. Ishrak Ahsan <1905045@ugrad.cse.buet.ac.bd>
2021-12-03 22:50:45 +0600

    Ready for submission

--- Offline 01/1905045.py
+++ Offline 01/1905045.py
@@ -8,8 +8,8 @@ Created on Tue Nov 30 22:17:20 2021
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from IPython.display import HTML
 
+# f(x) = x^3 - 0.18x^2 + 0.0004752
 def f(x):
     return x**3 - 0.18*x**2 + 4.752E-4
 
@@ -17,6 +17,7 @@ def f(x):
 # only find the solution    
 def bisection(lo, hi, eps, max_iterations):
     prev_mid = -1
+    
     for i in range(max_iterations):
         mid = (lo + hi) / 2
         
@@ -26,28 +27,27 @@ def bisection(lo, hi, eps, max_iterations):
         elif f(mid) * f(hi) < 0:
             # root lies between mid and hi
             lo = mid
+        
         if i == 0:
             prev_mid = mid
             continue
+        
         err = abs((mid - prev_mid) / mid) * 100
-        print(i, err)
         if err < eps:
             return mid
+        
         prev_mid = mid;
+    return prev_mid
+        
 
-
-# show table
-def bisection(lo, hi, iterations):
+# show error table
+def bisection_table(lo, hi, iterations):
     prev_mid = -1
-    rows = {'Iteration':[], 'x_l':[], 'x_u':[], 'x_m':[], '|ε_a|%':[], 'f(x_m)':[]}
+    rows = {'Iteration':[], 'Absolute Relative Approx. Error':[]}
     for i in range(iterations):
         rows['Iteration'].append(i+1)
-        rows['x_l'].append(lo)
-        rows['x_u'].append(hi)
         
         mid = (lo + hi) / 2
-        rows['x_m'].append(mid)
-        rows['f(x_m)'].append(f(mid))
         
         if f(lo) * f(mid) < 0:
             # root lies between lo and mid
@@ -55,21 +55,22 @@ def bisection(lo, hi, iterations):
         elif f(mid) * f(hi) < 0:
             # root lies between mid and hi
             lo = mid
-        if i == 0:
-            rows['|ε_a|%'].append('N/A')
-            prev_mid = mid
-            continue
-        err = abs((mid - prev_mid) / mid) * 100
-        rows['|ε_a|%'].append(err)
         
+        if i == 0:
+            prev_mid = mid
+            rows['Absolute Relative Approx. Error'].append('N/A')
+            continue
+        
+        err = abs((mid - prev_mid) / mid) * 100
+        rows['Absolute Relative Approx. Error'].append(err)
         prev_mid = mid;
-    pd.set_option("display.precision", 12)
+    
+    pd.set_option("display.precision", 20)
     df = pd.DataFrame(rows)
     print(df)
-    #display(df)
-    return prev_mid
     
-
+    
+# plot the graph of f(x) using matplotlib
 def plot_curve():
     x = np.arange(0, 0.12, 0.005)
     y = f(x) 
@@ -77,14 +78,34 @@ def plot_curve():
     plt.plot(x, y)
     
     plt.xlabel('x from 0 to 0.12')
-    plt.ylabel('x^3 - 0.18x^2 + 0.0004752')
+    plt.ylabel('y = x^3 - 0.18x^2 + 0.0004752')
     plt.title('Plot of f(x) = x^3 - 0.18x^2 + 0.0004752')
     
     plt.show()
     
-
+    
 if __name__ == '__main__':
+    """
+    Task 1
+    """
+    print('A graph of the function f(x) = x^3 - 0.18x^2 + 0.0004752 is plotted below:')
     plot_curve()
-    print(bisection, 0, 0.12, 0.5, 20)
-    print(bisection(0, 0.12, 20))
-
+    print()
+    
+    print('From the curve, it can be seen that f(x) changes its value from positive to negative between 0 and 0.12')
+    print('Approximate location of the root can be visually estimated as 0.06 m')
+    print()
+    
+    """
+    Task 2
+    """
+    print('Bisection method is applied to find the root...')
+    print('Approximate value of the root is: ', bisection(0, 0.12, 0.5, 20), 'm')
+    print()
+    
+    """
+    Task 3
+    """
+    print('A table showing the absolute relative approx. error after each iteration of the bisection method for up to 20 iterations:')
+    print()
+    bisection_table(0, 0.12, 20)
