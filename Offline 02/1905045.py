50224bcdb7d8482a24d9705b56b54aeaa9dd3d27
Md. Ishrak Ahsan <1905045@ugrad.cse.buet.ac.bd>
2022-01-07 16:28:56 +0600

    Ready for submission

--- Offline 02/1905045.py
+++ Offline 02/1905045.py
@@ -7,59 +7,50 @@ Created on Sat Jan  1 22:02:22 2022
 
 import numpy as np
 
-
-def print_matrix(A):
-    for i in range(A.shape[0]):
-        for j in range(A.shape[1]):
-            print('{0:0.4f}'.format(A[i][j]), end=' ')
-        print()
-    
-
 def partial_pivot(A, B, col, row):
     # find index of the max element staring from row in col
     idx = row
-    for i in range(row+1, A[row].size):
+    for i in range(row+1, A.shape[0]):
         if abs(A[i][col]) > abs(A[idx][col]):
             idx = i
     # swap
     A[[row, idx]] = A[[idx, row]]
     B[[row, idx]] = B[[idx, row]]
     
-
 def forward_elimination(A, B, pivot, showall):
-    for i in range(B.size):
+    for i in range(A.shape[1]):
         # i-th row is the initial pivot row
         if pivot:
             partial_pivot(A, B, i, i)
-        for j in range(i+1, B.size):
+        # i-th row is the pivot row, after possible swapping
+        for j in range(i+1, A.shape[0]):
             if A[j][i] != 0:
                 fac = A[j][i] / A[i][i]
                 A[j] = A[j] - fac * A[i]
                 B[j] = B[j] - fac * B[i]
             if showall:
+                print(f'After sub-step {j-i} of step {i+1}')
                 print('A:')
                 print_matrix(A)
                 print('B:')
                 print_matrix(B)
                 print()
 
-
 def back_substitution(A, B):
-    x = np.empty(B.size)
-    for i in range(B.size-1, -1, -1):
+    x = np.empty(A.shape[0])
+    for i in range(A.shape[0]-1, -1, -1):
         sum = 0
-        for j in range(i+1, B.size):
+        for j in range(i+1, A.shape[0]):
             sum = sum + A[i][j] * x[j]
         x[i] = (B[i] - sum) / A[i][i]
     return x
-
 
 def GaussianElimination(A, B, pivot=True, showall=True):
     forward_elimination(A, B, pivot, showall)
     x = back_substitution(A, B)
     return x
 
-
+# takes user input in row-major order
 def take_arr_input(n):
     arr = []
     for i in range(n):
@@ -69,11 +60,17 @@ def take_arr_input(n):
         arr.append(row)  
     return np.array(arr)
 
-
+# prints 2D matrix A in row-major order upto 4 decimal places
+def print_matrix(A):
+    for i in range(A.shape[0]):
+        for j in range(A.shape[1]):
+            print('{0:0.4f}'.format(A[i][j]), end=' ')
+        print()
+    
+# prints 1D solution matrix
 def print_output(x):
     for i in range(x.size):
         print('{0:0.4f}'.format(x[i]))
-
 
 if __name__ == '__main__':
     n = int(input())
@@ -84,4 +81,3 @@ if __name__ == '__main__':
     print('Solution:')
     print_output(x)
             
-    
