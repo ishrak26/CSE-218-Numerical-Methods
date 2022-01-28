2ed40dd0eb927ab299b25a677a2c81593f6e5fb3
Md. Ishrak Ahsan <1905045@ugrad.cse.buet.ac.bd>
2022-01-28 17:14:39 +0600

    Ready for submission

--- Offline 03 (Integration)/1905045.py
+++ Offline 03 (Integration)/1905045.py
@@ -2,7 +2,7 @@
 """
 Created on Thu Jan 27 18:48:54 2022
 
-@author: Md. Ishrak Ahsan
+@author: Md. Ishrak Ahsan (1905045)
 """
 
 import numpy as np
@@ -12,6 +12,7 @@ def f(x):
     C = 5E-4
     return (6.73*x + 6.725E-8 + 7.26E-4*C) / (3.62E-12 * x + 3.908E-8 * x * C)
 
+# Trapezoidal Rule for n equally spaced sub-intervals
 def trapezoid(n, a, b):
     h = (b-a)/n
     sum = 0
@@ -21,7 +22,9 @@ def trapezoid(n, a, b):
     sum += f(a) + f(b)
     return sum * h/2
 
+# Simpson's 1/3rd Rule for 2n equally spaced sub-intervals
 def simpson(n, a, b):
+    n *= 2
     h = (b-a)/n
     sum = f(a) + f(b)
     for i in range(1, n):
@@ -36,41 +39,56 @@ def plot_curve(x_0):
     x *= 1E-4
     y = np.empty(x.shape)
     for i in range(x.size):
-        y[i] = -simpson(10, x_0, x[i])
+        y[i] = -simpson(5, x_0, x[i])
     
     plt.plot(x, y, marker='o')
+    plt.grid()
+    plt.xlabel('Concentration of oxygen, x (mol/cm^3)')
+    plt.ylabel('Time, T (seconds)')
+    plt.title('Plot of Time vs Concentration of Oxygen')
     plt.show()
 
 if __name__=='__main__':
     x_0 = 1.22E-4
-    n = int(input("Enter the number of sub-intervals: ").strip())
+    n = int(input("Enter the value of n: ").strip())
+    print()
+    
     T = -trapezoid(n, x_0, x_0/2)
-    print(T)
-    print('Trapezoid:')
+    print(f'For {n} equally spaced sub-intervals, Trapezoidal Rule results in T = {T} seconds')
+    print()
+    print('For n equally spaced sub-intervals from n=1 to n=5, Trapezoidal Rule results:')
+    print('n\t\tT (seconds)\t\t\tAbsolute Approx. Relative Error (%)')
+    for i in range(60):
+        print('-', end='')
+    print()
     val = 0
     for i in range(1, 6):
         new_val = -trapezoid(i, x_0, x_0/2)
         if i > 1:
             err = abs((new_val - val) / new_val) * 100
-            print(i, new_val, err)
         else:
-            print(i, new_val, 'N/A')
+            err = 'Not Applicable'
         val = new_val
-    
+        print(f'{i}\t{val}\t\t\t{err}')
     print()
-    T = -simpson(2*n, x_0, x_0/2)
-    print(T)
-    print('Simpsons:')
+    
+    T = -simpson(n, x_0, x_0/2)
+    print(f"For {n*2} equally spaced sub-intervals, Simpson's 1/3rd Rule results in T = {T} seconds")
+    print()
+    print("For 2n equally spaced sub-intervals from n=1 to n=5, Simpson's 1/3rd Rule results:")
+    print('n\t\tT (seconds)\t\t\tAbsolute Approx. Relative Error (%)')
+    for i in range(60):
+        print('-', end='')
+    print()
     val = 0
     for i in range(1, 6):
-        new_val = -simpson(i*2, x_0, x_0/2)
+        new_val = -simpson(i, x_0, x_0/2)
         if i > 1:
             err = abs((new_val - val) / new_val) * 100
-            print(i, new_val, err)
         else:
-            print(i, new_val, 'N/A')
-        val = new_val 
+            err = 'Not Applicable'
+        val = new_val
+        print(f'{i}\t{val}\t\t\t{err}')
+    print()
     
     plot_curve(x_0)
-    
-    
